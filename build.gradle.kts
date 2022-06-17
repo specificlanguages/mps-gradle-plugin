25fa9d40ddcac3a2e348ee3d07087cfb950e1b25
Sergej Koščejev <sergej@koscejev.cz>
2022-06-17 23:48:44 +0200

    Simplify build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     java
     `java-gradle-plugin`
     `kotlin-dsl`
     `maven-publish`
-    kotlin("jvm") version "1.3.41"
     id("com.gradle.plugin-publish") version "0.16.0"
 }
 
@@ -20,23 +17,8 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 }
 
-configure<JavaPluginConvention> {
+java {
     sourceCompatibility = JavaVersion.VERSION_1_8
-}
-
-tasks {
-    wrapper {
-        gradleVersion = "5.6.2"
-        distributionType = Wrapper.DistributionType.ALL
-    }
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
 
 gradlePlugin {
