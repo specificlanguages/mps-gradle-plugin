dd557863b81887da3634d491375838422fdf6860
Sergej Koščejev <sergej@koscejev.cz>
2023-06-15 15:10:13 +0200

    feat: methods to add and configure a project dependency

--- subprojects/mps-gradle-plugin/CHANGELOG.md
+++ subprojects/mps-gradle-plugin/CHANGELOG.md
@@ -5,6 +5,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres
 to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.6.0]
+
+### Added
+- Methods on `StubConfiguration` to create, add, and configure a project dependency.
+
+### Changed
+- Corrected types of `StubConfiguration#dependency()` overloads.
+
 ## [1.5.0]
 
 ### Added
--- subprojects/mps-gradle-plugin/src/main/kotlin/com/specificlanguages/StubConfiguration.kt
+++ subprojects/mps-gradle-plugin/src/main/kotlin/com/specificlanguages/StubConfiguration.kt
@@ -7,10 +7,14 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.ExternalModuleDependency
+import org.gradle.api.artifacts.ModuleDependency
+import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
+import org.gradle.kotlin.dsl.add
 import org.gradle.kotlin.dsl.closureOf
+import org.gradle.kotlin.dsl.project
 
 abstract class StubConfiguration(private val project: Project, private val name: String) : Named {
     override fun getName(): String = name
@@ -33,7 +37,13 @@ abstract class StubConfiguration(private val project: Project, private val name:
         return project.dependencies.add(configuration.name, notation, config)
     }
 
-    fun dependency(notation: Any, config: (ExternalModuleDependency).() -> Unit): ExternalModuleDependency =
+    fun dependency(notation: String, config: (ExternalModuleDependency).() -> Unit): ExternalModuleDependency =
         dependency(notation, closureOf(config)) as ExternalModuleDependency
 
+    fun project(path: String, configuration: String? = null): ProjectDependency =
+        project.dependencies.project(path, configuration)
+    fun project(notation: Map<String, Any>) = project.dependencies.project(notation)
+
+    fun <T : ModuleDependency> dependency(dependency: T, config: T.() -> Unit): T =
+        project.dependencies.add(configuration.name, dependency, config)
 }
