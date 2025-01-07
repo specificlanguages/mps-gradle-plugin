7be73a7add30d1a93e1a26637eeaf5a100fe0dc4
Sergej Koščejev <sergej@koscejev.cz>
2025-01-07 11:02:14 +0100

    deps: update plugin versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 dependencies {
     fun plugin(id: String, version: String) = "$id:$id.gradle.plugin:$version"
 
-    implementation(plugin("com.gradle.plugin-publish", "1.2.1"))
+    implementation(plugin("com.gradle.plugin-publish", "1.3.0"))
     implementation(plugin("org.jetbrains.kotlin.jvm", embeddedKotlinVersion))
     implementation(plugin("org.gradle.kotlin.kotlin-dsl", "5.1.2"))
-    implementation(plugin("org.jetbrains.kotlinx.binary-compatibility-validator", "0.13.2"))
+    implementation(plugin("org.jetbrains.kotlinx.binary-compatibility-validator", "0.17.0"))
 }
