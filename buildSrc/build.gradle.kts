db52dab852e2cf171970b13e1edb97afa69d13ba
Sergej Koščejev <sergej@koscejev.cz>
2025-03-07 14:59:58 +0100

    build: upgrade to Gradle 8.13

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,6 @@ dependencies {
 
     implementation(plugin("com.gradle.plugin-publish", "1.3.0"))
     implementation(plugin("org.jetbrains.kotlin.jvm", embeddedKotlinVersion))
-    implementation(plugin("org.gradle.kotlin.kotlin-dsl", "5.1.2"))
+    implementation(plugin("org.gradle.kotlin.kotlin-dsl", "5.2.0"))
     implementation(plugin("org.jetbrains.kotlinx.binary-compatibility-validator", "0.17.0"))
 }
