d090f7085ce8542cbc95ff8734f661b5f7c0a720
Sergej Koščejev <sergej@koscejev.cz>
2026-01-13 14:36:54 +0100

    deps: update to Gradle Publishing Plugin 2.0.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 dependencies {
     fun plugin(id: String, version: String) = "$id:$id.gradle.plugin:$version"
 
-    implementation(plugin("com.gradle.plugin-publish", "1.3.0"))
+    implementation(plugin("com.gradle.plugin-publish", "2.0.0"))
     implementation(plugin("org.jetbrains.kotlin.jvm", embeddedKotlinVersion))
     implementation(plugin("org.gradle.kotlin.kotlin-dsl", expectedKotlinDslPluginsVersion))
     implementation(plugin("org.jetbrains.kotlinx.binary-compatibility-validator", "0.17.0"))
